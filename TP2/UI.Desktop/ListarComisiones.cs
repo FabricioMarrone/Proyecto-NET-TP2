7573a55c957dad12a22bc574888ba1a08abc05b8
villafloresmf <villafloresmf@gmail.com>
2014-10-06 20:51:12 -0300

    ListarComisiones
    Agregado de Interfaz ListarComisiones

---
+++ TP2/UI.Desktop/ListarComisiones.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Business.Logic;
+
+namespace UI.Desktop
+{
+    public partial class ListarComisiones : UI.Desktop.ListarBase
+    {
+        public ListarComisiones()
+        {
+            InitializeComponent();
+            this.GenerarColumnas();
+            this.Listar();
+        }
+
+        protected override void GenerarColumnas(){
+            DataGridViewColumn dgvColumn;
+
+            dgvColumn = this.CrearNuevaColumna("plan", "Plan", "id_plan");
+            this.dgvListar.Columns.Add(dgvColumn);
+
+            dgvColumn = this.CrearNuevaColumna("anio", "Año", "anio_especialidad");
+            this.dgvListar.Columns.Add(dgvColumn);
+
+            dgvColumn = this.CrearNuevaColumna("comision", "Comision", "desc_comision");
+            this.dgvListar.Columns.Add(dgvColumn);
+
+            dgvColumn = this.CrearNuevaColumna("id", "ID", "id_comision");
+            this.dgvListar.Columns.Add(dgvColumn);
+        }
+
+        public override void Listar()
+        {
+            ComisionLogic comisionesLogic = new ComisionLogic();
+            this.dgvListar.DataSource = comisionesLogic.GetAll();
+        }
+
+    }
+}
