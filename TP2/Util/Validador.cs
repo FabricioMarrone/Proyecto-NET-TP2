19a7b50821ff93e7acb36012bb53a06853481b9b
villafloresmf <villafloresmf@gmail.com>
2014-10-12 00:20:38 -0300

    Expresiones Regulares
    expresiones para validar datos de entrada.

--- TP2/Util/Validador.cs
+++ TP2/Util/Validador.cs
@@ -8,10 +8,53 @@ namespace Util
 {
     public class Validador
     {
-
-        public static bool ValidarClave(string clave)
+        public static bool ValidarNumericoPositivo(string numero, int min, int max)
         {
-            return clave.Length >= 8 && clave.Length <= 15;
+            string expresion = "^[0-9]";
+            if (max == 0)
+            {
+                expresion += ("{" + min + "}$");
+            }
+            else
+            {
+                expresion += "{" + min + "," + max + "}$";
+            }
+
+            return Regex.IsMatch(numero, expresion);
+        }
+
+        public static bool ValidarTelefono(string numero)
+        {
+            // minimo = 5 para telefonos de rosario sin codigo de area.
+            // maximo = 9 para telefonos con codigo de area. 
+            return ValidarNumericoPositivo(numero, 7, 11);
+        }
+
+        public static bool ValidarLegajo(string legajo)
+        {
+            // Preguntar
+            // {XX}-{XXXXX} con identificador especialidad? 
+            // {XXXXX} sin identificador especialidad?
+            return ValidarNumericoPositivo(legajo, 5, 0);
+        }
+
+        public static bool ValidarCadenaSoloTexto(string cadena)
+        {
+            return Regex.IsMatch(cadena, @"^(([a-zA-Z])+\s?)+$");
+        }
+
+        public static bool ValidarDireccion(string direccion)
+        {
+            return Regex.IsMatch(direccion, @"^(([a-zA-Z])+\s?)+([\d]+){1}(\s?(P|p)\d{1,3}\s?(D|d)\d{1,3})?$");
+        }
+
+        public static bool ValidarClave(string clave, string confirmacion)
+        {
+            if (!(Regex.IsMatch(clave, @"^([a-zA-Z0-9]{8,15})$")) || !(clave.Equals(confirmacion)))
+            {
+                return false;
+            }
+            return true;
         }
 
         public static bool ValidarEMail(string email)
@@ -19,9 +62,14 @@ namespace Util
             return Regex.IsMatch(email, @"^([\w-]+\.)*?[\w-]+@[\w-]+\.([\w-]+\.)*?[\w]+$");
         }
 
-        public static bool ValidarCadenaSoloTexto(string cadena) 
+        public static bool ValidarFecha(string fecha)
         {
-            return Regex.IsMatch(cadena,@"");
+            return Regex.IsMatch(fecha, @"^([1-31]){1,2}\/([1-12]){1,2}\/([0-9]){4}$");
+        }
+
+        public static bool ValidarCamposNoVacios(string campo)
+        {
+            return campo.Trim() == "";
         }
 
     }
