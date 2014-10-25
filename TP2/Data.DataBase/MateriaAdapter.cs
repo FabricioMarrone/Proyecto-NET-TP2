ccb8b6c8c6aefe026f27b54f58fd2e1f380d4a43
villafloresmf <villafloresmf@gmail.com>
2014-10-25 18:54:13 -0300

    Materias para Inscripcion
    Metodo que devuelve materias para inscripcion. Filtrando por Plan y por
    Materias que el alumno ya esta regular o inscripto

--- TP2/Data.DataBase/MateriaAdapter.cs
+++ TP2/Data.DataBase/MateriaAdapter.cs
@@ -64,5 +64,30 @@ namespace Data.DataBase
                 academiaContext.SaveChanges();
             }            
         }
+
+        public List<materia> GetMateriasParaInscripcion(persona oPersona)
+        {
+            using (AcademiaEntities academiaContext = new AcademiaEntities())
+            {
+                // Sub consulta: Devuelve los id_materia de las Materias para las cuales el alumno
+                // ya esta Inscripto o Regular.
+                var subQuery = from insc in academiaContext.alumnos_inscripciones
+                               where (insc.id_alumno == oPersona.id_persona) &&
+                                     (insc.condicion == "Regular" || insc.condicion == "Inscripto")
+                               select insc.curso.id_materia;
+
+                /* Consulta que devuelve las materias de acuerdo a la especialidad y
+                 * plan de la persona(Alumno). Asi nos evitamos que puedan inscribirse
+                 * a materias de otras especialidades que no son de la suya.
+                 * Tampoco se incluiran las materias a las cuales el alumno ya esta
+                 * Inscrito o Regular.
+                */
+                var queryMaterias = (from mat in academiaContext.materias
+                                     where (mat.id_plan == oPersona.id_plan) &&
+                                     !(subQuery.Contains(mat.id_materia))
+                                     select mat).ToList();
+                return queryMaterias;
+            }
+        }
     }
 }
