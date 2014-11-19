d856c9f4728443242862fa83b45c35de9cc8d56b
villafloresmf <villafloresmf@gmail.com>
2014-11-19 15:20:56 -0300

    Menu de Admin Listo

--- TP2/UI.Web/Site.Master.designer.cs
+++ TP2/UI.Web/Site.Master.designer.cs
@@ -85,6 +85,60 @@ namespace UI.Web {
         protected global::System.Web.UI.WebControls.LinkButton linkMenuOption3;
         
         /// <summary>
+        /// Control PersonasLB.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton PersonasLB;
+        
+        /// <summary>
+        /// Control EspecialidadesLB.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton EspecialidadesLB;
+        
+        /// <summary>
+        /// Control PlanesLB.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton PlanesLB;
+        
+        /// <summary>
+        /// Control MateriasLB.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton MateriasLB;
+        
+        /// <summary>
+        /// Control ComisionesLB.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton ComisionesLB;
+        
+        /// <summary>
+        /// Control CursosLB.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton CursosLB;
+        
+        /// <summary>
         /// Control bodyContentPlaceHolder.
         /// </summary>
         /// <remarks>
--- TP2/UI.Web/Site.master.cs
+++ TP2/UI.Web/Site.master.cs
@@ -19,6 +19,49 @@ namespace UI.Web
             }
         }
 
+        public void showMenu(int tipo)
+        {
+            //LinkButton linkMenuOption1 = (LinkButton)FindControlRecursive(Master, "linkMenuOption1");
+            //LinkButton linkMenuOption2 = (LinkButton)FindControlRecursive(Master, "linkMenuOption2");
+            //LinkButton linkMenuOption3 = (LinkButton)FindControlRecursive(Master, "linkMenuOption3");
+            linkMenuOption0.Text = "Cerrar sesión";
+            linkMenuOption1.Text = "Modificar datos";
+            linkMenuOption1.Visible = true;
+            switch (tipo)
+            {
+                case (int)persona.tipo.Alumno:
+                    linkMenuOption2.Text = "Estado academico";
+                    linkMenuOption3.Text = "Inscripcion a materia";
+                    linkMenuOption2.Visible = true;
+                    linkMenuOption3.Visible = true;
+                    break;
+                case (int)persona.tipo.Admin:
+                    linkMenuOption2.Text = "Usuarios";
+                    linkMenuOption2.Visible = true;
+
+                    linkMenuOption3.Text = "Otra Opciones de ADMIN";
+                    linkMenuOption3.Visible = false;
+
+                    //Controles de Admin para ABMs
+                    this.PersonasLB.Visible = true;
+                    this.EspecialidadesLB.Visible = true;
+                    this.PlanesLB.Visible = true;
+                    this.MateriasLB.Visible = true;
+                    this.ComisionesLB.Visible = true;
+                    this.CursosLB.Visible = true;
+                    
+                    break;
+                case (int)persona.tipo.Profesor:
+                    linkMenuOption2.Text = "Cargar notas";
+                    linkMenuOption3.Text = "Inscripcion a curso";
+                    linkMenuOption2.Visible = true;
+                    linkMenuOption3.Visible = true;
+                    break;
+            }//end switch
+        }
+
+
+        #region Manejadores Eventos Link Buttons
         protected void linkMenuOption0_Click(object sender, EventArgs e)
         {
             if (this.Session[Global.USUARIO_ACTUAL] == null)
@@ -26,7 +69,7 @@ namespace UI.Web
                 //No hay sesion iniciada
                 Response.Redirect("Home.aspx");
             }
-            else 
+            else
             {
                 //Hay sesion iniciada y se desea "cerrar la sesion".
                 this.Session[Global.USUARIO_ACTUAL] = null;
@@ -49,7 +92,7 @@ namespace UI.Web
         protected void linkMenuOption2_Click(object sender, EventArgs e)
         {
             persona per = (persona)this.Session[Global.PERSONA_ACTUAL];
-            switch (per.tipo_persona) 
+            switch (per.tipo_persona)
             {
                 case (int)persona.tipo.Alumno:
                     //Estado academico
@@ -75,7 +118,7 @@ namespace UI.Web
                     Response.Redirect("InscripcionAmateria.aspx");
                     break;
                 case (int)persona.tipo.Admin:
-                 
+
                     break;
                 case (int)persona.tipo.Profesor:
                     //Inscripcion a curso
@@ -84,38 +127,37 @@ namespace UI.Web
             }
         }
 
-        public void showMenu(int tipo)
+        protected void PersonasLB_Click(object sender, EventArgs e)
         {
-            //LinkButton linkMenuOption1 = (LinkButton)FindControlRecursive(Master, "linkMenuOption1");
-            //LinkButton linkMenuOption2 = (LinkButton)FindControlRecursive(Master, "linkMenuOption2");
-            //LinkButton linkMenuOption3 = (LinkButton)FindControlRecursive(Master, "linkMenuOption3");
-            linkMenuOption0.Text = "Cerrar sesión";
-            linkMenuOption1.Text = "Modificar datos";
-            linkMenuOption1.Visible = true;
-            switch (tipo)
-            {
-                case (int)persona.tipo.Alumno:
-                    linkMenuOption2.Text = "Estado academico";
-                    linkMenuOption3.Text = "Inscripcion a materia";
-                    linkMenuOption2.Visible = true;
-                    linkMenuOption3.Visible = true;
-                    break;
-                case (int)persona.tipo.Admin:
-                    linkMenuOption2.Text = "Lista de Usuarios";
-                    linkMenuOption2.Visible = true;
-
-                    linkMenuOption3.Text = "Otra Opciones de ADMIN";
-                    linkMenuOption3.Visible = true;
-                    
-                    break;
-                case (int)persona.tipo.Profesor:
-                    linkMenuOption2.Text = "Cargar notas";
-                    linkMenuOption3.Text = "Inscripcion a curso";
-                    linkMenuOption2.Visible = true;
-                    linkMenuOption3.Visible = true;
-                    break;
-            }//end switch
+            this.Response.Redirect("~/Personas.aspx");
         }
+
+        protected void EspecialidadesLB_Click(object sender, EventArgs e)
+        {
+            this.Response.Redirect("~/Especialidades.aspx");
+        }
+
+        protected void PlanesLB_Click(object sender, EventArgs e)
+        {
+            this.Response.Redirect("~/Planes.aspx");
+        }
+
+        protected void MateriasLB_Click(object sender, EventArgs e)
+        {
+            this.Response.Redirect("~/Materias.aspx");
+        }
+
+        protected void ComisionesLB_Click(object sender, EventArgs e)
+        {
+            this.Response.Redirect("~/Comisiones.aspx");
+        }
+
+        protected void CursosLB_Click(object sender, EventArgs e)
+        {
+            this.Response.Redirect("~/Cursos.aspx");
+        }
+        #endregion
+
 
         public void occultMenu()
         {
@@ -125,9 +167,12 @@ namespace UI.Web
             linkMenuOption1.Visible = false;
             linkMenuOption2.Visible = false;
             linkMenuOption3.Visible = false;
+            this.PersonasLB.Visible = false;
+            this.EspecialidadesLB.Visible = false;
+            this.PlanesLB.Visible = false;
+            this.MateriasLB.Visible = false;
+            this.ComisionesLB.Visible = false;
+            this.CursosLB.Visible = false;
         }
-
-       
-
     }//end class
 }
